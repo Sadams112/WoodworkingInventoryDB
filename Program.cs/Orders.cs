660c64220ffdf2cb5cddd08809751a1d8ab5ece1
Spencer Adams <spenceradams09@gmail.com>
2023-11-06 18:17:49 -0500

    2nd Commit

--- Program.cs/Orders.cs
+++ Program.cs/Orders.cs
@@ -1,9 +1,9 @@
 namespace Program.cs;
 
-public class Orders
+public class Order
 {
     public int PO { get; set; }
-    public string Job { get; set; }
+    public string JobName { get; set; }
     public List<OrderItem> Items { get; set; }
     
 }
--- Program.cs/Program.cs
+++ Program.cs/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using Program.cs;
-
+using System.Linq;
 
 class program
 {
@@ -54,15 +54,57 @@ class program
                         case "2":
                             Console.Write(" Enter item category you would like to search for (e.g., Laminate, Hardware, Board, etc: ");
                             string itemCategory = Console.ReadLine();
-
+                            
+                            //Using Linq to query Orders table
                             var ordersByCategory = context.Orders
                                 .Where(order => order.Items.Any(item => item.Category == itemCategory));
+                                .ToList();
+
+                            if (ordersByCategory.Any())
+                            {
+                                Console.WriteLine(" Search Results: ");
+                                DisplayOrders(ordersByCategory);
+                            }
+                            else
+                            {
+                                Console.WriteLine($" No orders found within the category: {itemCategory}");
+                            }
+
+                            break;
                 }
                 
                 
 
 
             }
+        }
+    }
+
+    static void DisplayOrders(IEnumerable<Order> orders)
+    {
+        if (orders.Any())
+        {
+            Console.WriteLine(" Search Results: ");
+            foreach (var order in orders)
+            {
+                Console.WriteLine($"OrderPO: {order.PO}");
+                Console.WriteLine($"Job Name: {order.JobName}");
+                Console.WriteLine($"Items:");
+
+                foreach (var item in order.Items)
+                    
+                {
+                    Console.WriteLine($" Category: {item.Category}");
+                    Console.WriteLine($" Description: {item.Description}");
+                    Console.WriteLine($" Quantity: {item.Quantity}");
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine(" No matching orders found. ");
         }
     }
 }
