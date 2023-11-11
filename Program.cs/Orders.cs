47baf890d43dce3c3a1467c33ce22d5ec5f5a94e
Spencer Adams <spenceradams09@gmail.com>
2023-11-11 14:57:09 -0500

    Updated Code

--- Program.cs/Orders.cs
+++ Program.cs/Orders.cs
@@ -5,5 +5,5 @@ public class Order
     public int PO { get; set; }
     public string JobName { get; set; }
     public List<OrderItem> Items { get; set; }
-    
+    public string manufacturerName { get; set; }
 }
--- Program.cs/Program.cs
+++ Program.cs/Program.cs
@@ -52,33 +52,67 @@ class program
                         break;
                         
                         case "2":
-                            Console.Write(" Enter item category you would like to search for (e.g., Laminate, Hardware, Board, etc: ");
-                            string itemCategory = Console.ReadLine();
-                            
-                            //Using Linq to query Orders table
-                            var ordersByCategory = context.Orders
-                                .Where(order => order.Items.Any(item => item.Category == itemCategory));
-                                .ToList();
-
-                            if (ordersByCategory.Any())
-                            {
-                                Console.WriteLine(" Search Results: ");
-                                DisplayOrders(ordersByCategory);
-                            }
-                            else
-                            {
-                                Console.WriteLine($" No orders found within the category: {itemCategory}");
-                            }
-
+                            SearchOrders(context);
                             break;
+                            
                 }
-                
-                
-
-
             }
         }
     }
+
+
+
+    static void SearchOrders(OrderDbConext context)
+    {
+        bool searchExit = false;
+
+        while (!searchExit)
+        {
+            Console.WriteLine("Search Orders");
+            Console.WriteLine("1. Search by manufacturer name ");
+            Console.WriteLine("2. Search by item Category ");
+            Console.WriteLine("3. Back to main menu ");
+            Console.Write("Enter your Choice: ");
+            string searchChoice = Console.ReadLine();
+
+
+            switch (searchChoice)
+            {
+                case "1":
+                    Console.Write("Enter manufacturer name to search for: ");
+                    string manufacturerName = Console.ReadLine();
+
+                    var ordersByManufacturer = context.Orders
+                        .Where(order => order.ManufacturerName.Contains(manufacturerName))
+                        .ToList();  // Using ToList to materialize the results
+
+                    DisplayOrders(ordersByManufacturer);
+                    break;
+                
+                case "2":
+                    Console.Write("Enter item category to search for (e.g., Laminate, Hardware, Board, etc.): ");
+                    string itemCategory = Console.ReadLine();
+
+                    var ordersByCategory = context.Orders
+                        .Where(order => order.Items.Any(item => item.Category == itemCategory))
+                        .ToList();  // Using ToList to materialize the results
+
+                    DisplayOrders(ordersByCategory);
+                    break;
+
+                case "3":
+                    searchExit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+
+    }
+
+    
 
     static void DisplayOrders(IEnumerable<Order> orders)
     {
@@ -107,7 +141,38 @@ class program
             Console.WriteLine(" No matching orders found. ");
         }
     }
+    
+    static void SaveOrdersToFile(IEnumerable<Order> orders, string filePath)
+    {
+        try
+        {
+            using (var writer = new System.IO.StreamWriter(filePath))
+            {
+                foreach (var order in orders)
+                {
+                    writer.WriteLine($"Order PO: {order.PO}");
+                    writer.WriteLine($"Manufacturer Name: {order.manufacturerName}");
+                    writer.WriteLine("Items:");
+                    foreach (var item in order.Items)
+                    {
+                        writer.WriteLine($"  Category: {item.Category}");
+                        writer.WriteLine($"  Description: {item.Description}");
+                        writer.WriteLine($"  Quantity: {item.Quantity}");
+                    }
+                    writer.WriteLine();
+                }
+            }
+
+            Console.WriteLine($"Orders successfully saved to {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error saving orders to file: {ex.Message}");
+        }
+    }
+
 }
+
 
 
 
